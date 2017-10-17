a234dec2807130286f7be922b3e3bf73407dba7e
uchang7194 <suyoung7194@gmail.com>
2017-10-17 22:53:07 +0900

    scaffolding

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './scss/stylesheets.scss'; 
+
+ReactDOM.render(<App />, document.querySelector('#App'));
