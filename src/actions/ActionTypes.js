a9d7e705c0f516b41b61a80660b2bfb8efe58819
Dasom Kim <babam1012@gmail.com>
2017-10-22 17:45:51 +0900

    [다솜][테스트]

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,3 +1,5 @@
+// DB가져오는 action
 export const FETCH_DB = 'FETCH_DB';
+// list 정렬하는 action
 export const LIST_SORT_BY_LATEST ='LIST_SORT_BY_LATEST';
 export const LIST_SORT_BY_POPULAR ='LIST_SORT_BY_POPULAR';
